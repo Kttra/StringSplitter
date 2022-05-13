04843ef666519a700f1591bf5d656cccc2468ad3
Kttra <100814612+Kttra@users.noreply.github.com>
2022-05-12 18:56:42 -0700

    Updated to not include whitespace or empty strings
    Made it so that white space and empty strings are not added to the word vector. Got rid of using namespace std. Converted blocks of code into functions to transfer to other programs more easily.

--- main.cpp
+++ main.cpp
@@ -4,67 +4,85 @@
     then splits the line into words and outputs it to the user.
 
     It will assume that spaces indicates a new word. This program
-    is made to test strings and vectors. This code is used for preparation
-    in order to update the time adder project.
+    is made to test strings and vectors. This code is also used for preparation
+    in order to update my time adder project.
+
+    Update 5/12/2022
+    Made it so white space and empty strings are not added to the word vector.
+    Got rid of using namespace std. Converted blocks of code into functions to
+    transfer to other projects more easily
 */
 
 #include <iostream>
 #include <vector>
 #include <string>
 
-using namespace std;
+std::vector<std::string> SplitStr(std::string strToSplit);
+void PrintWords(std::vector<std::string> wordList);
 
 int main(){
-    string userInput;
-    vector<string> wordList;
+    std::string userInput;
 
     //Loops the whole program
     while(true){
-        int wordNum = 0;
-        string word;
-
         //Getting userInput
         std::cout << "Enter a line of text: ";
-        getline(cin, userInput);
+        getline(std::cin, userInput);
 
-        //length of the userInput
-        int length = userInput.length();
-
-        //Separating the words into a vector
-        for(int i = 0; i < length; i++){
-            if(userInput[i] != ' '){
-                word = word + userInput[i];
-            }
-            else{
-                wordList.push_back(word);
-                word = "";
-                wordNum++;
-            }
-        }
-
-        //Because there is not a space at the end, we need to
-        //Add the last word manually
-        wordList.push_back(word);
-
-        //Outputting the number of words
-        if(wordList.size() == 1){
-            cout << endl << "There is only " << wordList.size() << " word in your line. Here is the word: " << endl;
-        }
-        else{
-            cout << endl << "There are " << wordList.size() << " words in your line. Here are the words: " << endl;
-        }
-
-        //Outputting the words
-        for(int i = 0; i < wordList.size(); i++){
-            cout << "Word " << i+1 << ": ";
-            cout << wordList[i] << endl;
-        }
-        cout << endl;
-
-        //Reset everything
-        word = "";
-        wordList.clear();
+        //Split the string
+        SplitStr(userInput);
     }
 
     return 0;
+}
+
+//Split the string at every space and append each word to a vector
+std::vector<std::string> SplitStr(std::string strToSplit){
+    std::vector<std::string> wordList;
+    int wordNum = 0;
+    std::string word;
+
+    //Length of the string
+    int length = strToSplit.length();
+
+    //Separating the words into a vector
+    for(int i = 0; i < length; i++){
+        //If the char is not a space, append it
+        if(strToSplit[i] != ' '){
+            word = word + strToSplit[i];
+        }
+        //Make sure that the word is not empty
+        else if(!word.empty()){
+            wordList.push_back(word);
+            word = "";
+            wordNum++;
+        }
+    }
+
+    //Because there is not a space at the end, we need to add the last word manually
+    //Also need to make sure that it is not just white space
+    if(word.find_first_not_of(' ') != std::string::npos){
+        wordList.push_back(word);
+    }
+
+    PrintWords(wordList);
+
+    return wordList;
+}
+
+void PrintWords(std::vector<std::string> wordList){
+    //Outputting the number of words
+    if(wordList.size() == 1){
+        std::cout << '\n' << "There is only 1 word in your line. Here is the word: " << '\n';
+    }
+    else{
+        std::cout << '\n' << "There are " << wordList.size() << " words in your line. Here are the words: " << '\n';
+    }
+
+    //Outputting the words
+    for(int i = 0; i < wordList.size(); i++){
+        std::cout << "Word " << i+1 << ": ";
+        std::cout << wordList[i] << '\n';
+    }
+    std::cout << std::endl;
 }
